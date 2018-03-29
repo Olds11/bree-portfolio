2d6001ab552f3ba01d956557fdf304b2cbfd5716
Bree <bree@Pink.local>
2018-03-29 15:20:25 -0700

    scroll and reveal features added

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,5 @@
+//Toggle mobile menu
+
 class MobileMenu {
 	constructor() {
 		this.menuIcon = $(".site-header__menu-icon");
@@ -18,4 +20,86 @@ class MobileMenu {
 }
 
 
-var mobileMenu = new MobileMenu;
+var mobileMenu = new MobileMenu();
+
+
+//reveal on scroll
+
+class RevealOnScroll {
+	constructor(els, offset) {
+		this.itemsToReveal = els;
+		this.offsetPercentage = offset;
+		this.hideInitially();
+		this.createWaypoints();
+
+	}
+
+	hideInitially() {
+		this.itemsToReveal.addClass("reveal-item");
+	}
+
+	createWaypoints() {
+		var that = this;
+		this.itemsToReveal.each(function() {
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,
+				handler: function() {
+					$(currentItem).addClass("reveal-item--is-visible");
+				},
+				offset: that.offsetPercentage
+			});
+		});
+	}
+}
+
+var revealOnScrollExpertise = new RevealOnScroll($(".expert-item"), "80%");
+
+
+//sticky header
+
+
+class StickyHeader {
+	constructor() {
+		this.siteHeader = $(".site-header");
+		this.headerTriggerElement = $(".large-hero__title");
+		this.createHeaderWaypoint();
+
+	}
+
+	createHeaderWaypoint() {
+		var that = this;
+		new Waypoint({
+			element: this.headerTriggerElement[0],
+			handler: function(direction) {
+				if (direction == "down") {
+					that.siteHeader.addClass("site-header--dark");
+				} else {
+					that.siteHeader.removeClass("site-header--dark");
+				}
+			}
+		});
+	}
+
+}
+
+var stickyHeader = new StickyHeader();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
