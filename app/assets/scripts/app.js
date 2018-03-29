d4f04e8e5ca02ae3cd14b6bce4ff8b1d5f3bbb08
Bree <bree@Pink.local>
2018-03-29 16:02:35 -0700

    smooth scroll added

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -63,8 +63,17 @@ class StickyHeader {
 	constructor() {
 		this.siteHeader = $(".site-header");
 		this.headerTriggerElement = $(".large-hero__title");
+		this.pageSections = $(".page-section");
+		this.headerLinks = $(".primary-nav a");
 		this.createHeaderWaypoint();
+		this.createPageSectionWaypoints();
+		this.resetPageSectionWaypoints();
+		this.smoothScroll();
 
+	}
+
+	smoothScroll() {
+		this.headerLinks.smoothScroll();
 	}
 
 	createHeaderWaypoint() {
@@ -81,10 +90,55 @@ class StickyHeader {
 		});
 	}
 
+	createPageSectionWaypoints() {
+		var that = this;
+		this.pageSections.each(function() {
+			var currentPageSection = this;
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if (direction == "down") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					} 
+				},
+				offset: "18%"
+
+			}); 
+			new Waypoint({
+				element: currentPageSection,
+				handler: function(direction) {
+					if (direction == "up") {
+						var matchingHeaderLink = currentPageSection.getAttribute("data-matching-link");
+						that.headerLinks.removeClass("is-current-link");
+						$(matchingHeaderLink).addClass("is-current-link");
+					} 
+				},
+				offset: "-50%"
+
+			}); 
+		});
+	}
+
+	resetPageSectionWaypoints() {
+        var that = this;
+        new Waypoint({
+            element: this.headerTriggerElement[0],
+            handler: function(direction) {
+               if (direction == "up") {
+                   that.headerLinks.removeClass("is-current-link");
+               }
+            },
+            offset: "-30%"
+        });
+
+		
+	}
+
 }
 
 var stickyHeader = new StickyHeader();
-
 
 
 
